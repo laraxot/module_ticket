2567fa5e7b7011ea55119113a370fffde1c1c8a9
Malebestia <malebestia@yahoo.it>
2022-08-15 12:34:56 +0200

    .

---
+++ Models/Panels/Policies/_ModulePanelPolicy.php
@@ -0,0 +1,11 @@
+<?php
+namespace Modules\Ticket\Models\Panels\Policies;
+
+use Illuminate\Auth\Access\HandlesAuthorization;
+use Modules\LU\Models\User as User;
+use Modules\Ticket\Models\Panels\Policies\_ModulePanelPolicy as Panel;
+
+use Modules\Xot\Models\Panels\Policies\XotBasePanelPolicy;
+
+class _ModulePanelPolicy extends XotBasePanelPolicy {
+}
---
+++ Models/Panels/_ModulePanel.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Modules\Ticket\Models\Panels;
+
+use Modules\Xot\Models\Panels\XotBasePanel;
+
+/**
+ * Class _ModulePanel.
+ */
+class _ModulePanel extends XotBasePanel {
+    public function actions(): array {
+        return [
+        ];
+    }
+}
