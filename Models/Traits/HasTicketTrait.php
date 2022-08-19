ec39ca144b8f454e1c325fbf78e80043e545cdc7
Malebestia <malebestia@yahoo.it>
2022-08-19 18:01:56 +0200

    .

--- Models/Traits/HasTicketTrait.php
+++ Models/Traits/HasTicketTrait.php
@@ -4,27 +4,12 @@ declare(strict_types=1);
 
 namespace Modules\Ticket\Models\Traits;
 
-use Illuminate\Database\Eloquent\Builder;
-// //use Laravel\Scout\Searchable;
-
-// ----- models------
-use Illuminate\Support\Facades\Auth;
-// ----- relations------
 use Illuminate\Database\Eloquent\Relations\HasMany;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
-// ---- services -----
-//use Modules\Rating\Models\Rating;
-use Modules\Xot\Services\PanelService as Panel;
-use Modules\Ticket\Models\Ticket;
+use Illuminate\Support\Facades\Auth;
 use Modules\LU\Models\User;
-
-
-// ------ traits ---
+use Modules\Ticket\Models\Ticket;
 
 trait HasTicketTrait {
-
-    // ------- RELATIONSHIP ----------
-
     public function tickets(): HasMany {
         return Auth::user()->hasMany(Ticket::class);
     }
@@ -32,5 +17,4 @@ trait HasTicketTrait {
     /*tickets ha user_id
     profiles ha user_id
     users ha id*/
-
-}// end model
+}
