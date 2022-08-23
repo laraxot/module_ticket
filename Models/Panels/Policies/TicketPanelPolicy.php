1a1ea5742614d50a4896568345e61db7f9340e9e
marco76tv <marco.sottana@gmail.com>
2022-08-23 13:50:31 +0200

    up

--- Models/Panels/Policies/TicketPanelPolicy.php
+++ Models/Panels/Policies/TicketPanelPolicy.php
@@ -1,11 +1,18 @@
 <?php
+
+declare(strict_types=1);
+
 namespace Modules\Ticket\Models\Panels\Policies;
 
-use Illuminate\Auth\Access\HandlesAuthorization;
-use Modules\LU\Models\User as User;
-use Modules\Ticket\Models\Panels\Policies\TicketPanelPolicy as Panel;
-
+use Modules\Xot\Contracts\PanelContract;
+use Modules\Xot\Contracts\UserContract;
 use Modules\Xot\Models\Panels\Policies\XotBasePanelPolicy;
 
 class TicketPanelPolicy extends XotBasePanelPolicy {
+    /**
+     * Undocumented function.
+     */
+    public function create(?UserContract $user, PanelContract $panel): bool {
+        return true;
+    }
 }
