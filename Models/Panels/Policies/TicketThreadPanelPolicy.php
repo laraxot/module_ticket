708237bedb463d5f9c927a59d5f4a56e5899127c
Davide Cavallini <davidecavallini1987@gmail.com>
2023-01-19 10:55:11 +0100

    .

--- Models/Panels/Policies/TicketThreadPanelPolicy.php
+++ Models/Panels/Policies/TicketThreadPanelPolicy.php
@@ -1,9 +1,6 @@
 <?php
-namespace Modules\Ticket\Models\Panels\Policies;
 
-use Illuminate\Auth\Access\HandlesAuthorization;
-use Modules\LU\Models\User as User;
-use Modules\Ticket\Models\Panels\Policies\TicketThreadPanelPolicy as Post; 
+namespace Modules\Ticket\Models\Panels\Policies;
 
 use Modules\Cms\Models\Panels\Policies\XotBasePanelPolicy;
 
