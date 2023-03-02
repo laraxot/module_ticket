fa28e52759886a1c4e742e424f9f35c0012647e1
marco76tv <marco.sottana@gmail.com>
2023-03-02 11:12:52 +0100

    up

--- Http/Livewire/Modal/Ticket/Create.php
+++ Http/Livewire/Modal/Ticket/Create.php
@@ -1,46 +1,47 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Modules\Ticket\Http\Livewire\Modal\Ticket;
 
+use Illuminate\Contracts\Support\Renderable;
 use Modules\Blog\Models\Category;
 use Modules\Ticket\Models\Ticket;
-use Illuminate\Contracts\Support\Renderable;
 use Modules\Xot\Actions\Model\StoreAction;
 use WireElements\Pro\Components\Modal\Modal;
 
- class Create extends Modal
- {
+class Create extends Modal {
     public array $form_data = [];
-     public function render():Renderable
-     {
+
+    public function render(): Renderable {
         /** @phpstan-var view-string */
         $view = 'ticket::livewire.modal.ticket.create';
 
-        $category_opts=[];
-        $categories=Category::ofType('ticket')->get();
-        if($categories->count()==0){
-            $ticket = Ticket::make();
+        $category_opts = [];
+        $categories = Category::ofType('ticket')->get();
+        if (0 == $categories->count()) {
+            // phpstan mi dice di cambiarlo così:
+            $ticket = new Ticket();
+            // $ticket = Ticket::make();
             $ticket->id = 0;
             $config_categories = config('ticket.categories');
-            //dddx($config_categories);
+            // dddx($config_categories);
             $ticket->attachCategories($config_categories);
-
         }
 
-        $category_opts=$categories->pluck('name','id')->all();
+        $category_opts = $categories->pluck('name', 'id')->all();
 
         $view_params = [
-            'view'=>$view,
-            'category_opts'=>$category_opts,
+            'view' => $view,
+            'category_opts' => $category_opts,
         ];
-        return view($view,$view_params);
-     }
 
+        return view($view, $view_params);
+    }
 
-
-     public function save(){
+    public function save() {
         $model = app(Ticket::class);
         $data = $this->form_data;
-        $res=app(StoreAction::class)->execute($model, $data,[]);
-     }
- }
+        $res = app(StoreAction::class)->execute($model, $data, []);
+    }
+}
--- Observers/TicketActionObserver.php
+++ Observers/TicketActionObserver.php
@@ -1,31 +1,34 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Modules\Ticket\Observers;
 
-use Modules\Ticket\Notifications\DataChangeEmailNotification;
-use Modules\Ticket\Notifications\AssignedTicketNotification;
-use Modules\Ticket\Models\Ticket;
 use Illuminate\Support\Facades\Notification;
+use Modules\LU\Models\User;
+use Modules\Ticket\Models\Ticket;
+use Modules\Ticket\Notifications\AssignedTicketNotification;
+use Modules\Ticket\Notifications\DataChangeEmailNotification;
 
-class TicketActionObserver
-{
-    public function created(Ticket $model)
-    {
-        $data  = ['action' => 'New ticket has been created!', 'model_name' => 'Ticket', 'ticket' => $model];
-        $users = \App\User::whereHas('roles', function ($q) {
-            return $q->where('title', 'Admin');
-        })->get();
-        Notification::send($users, new DataChangeEmailNotification($data));
+class TicketActionObserver {
+    public function created(Ticket $model) {
+        $data = ['action' => 'New ticket has been created!', 'model_name' => 'Ticket', 'ticket' => $model];
+        // non so se sia giusto fatto così
+        // i ruoli li abbiamo sul profilo e non sull'utente quindi non saprei come sistemarlo bene.
+        /* $users = User::whereHas('roles', function ($q) {
+             return $q->where('title', 'Admin');
+         })->get();*/
+
+        return collect([]);
+        // le notifiche mancano. probabilmente vanno fatte con le nostre notifiche. Intanto commento (oggi 2021-03-02)
+        // Notification::send($users, new DataChangeEmailNotification($data));
     }
 
-    public function updated(Ticket $model)
-    {
-        if($model->isDirty('assigned_to_user_id'))
-        {
+    public function updated(Ticket $model) {
+        if ($model->isDirty('assigned_to_user_id')) {
             $user = $model->assigned_to_user;
-            if($user)
-            {
-                Notification::send($user, new AssignedTicketNotification($model));
+            if ($user) {
+                // Notification::send($user, new AssignedTicketNotification($model));
             }
         }
     }
