b322c6ced1ef12d16f6127360ab75dba6ee51c49
marco76tv <marco.sottana@gmail.com>
2023-04-04 11:26:16 +0200

    up

--- Models/Panels/Policies/HomePanelPolicy.php
+++ Models/Panels/Policies/HomePanelPolicy.php
@@ -1,14 +1,17 @@
 <?php
 namespace Modules\Ticket\Models\Panels\Policies;
 
-use Illuminate\Auth\Access\HandlesAuthorization;
-use Modules\LU\Models\User as User;
-use Modules\Ticket\Models\Panels\Policies\HomePanelPolicy as Post; 
+use Modules\Xot\Contracts\UserContract;
+use Modules\Cms\Contracts\PanelContract;
 
 use Modules\Cms\Models\Panels\Policies\XotBasePanelPolicy;
 
 class HomePanelPolicy extends XotBasePanelPolicy {
     public function artisan(?UserContract $user, PanelContract $panel): bool {
+        return true;
+    }
+
+    public function dashboard(UserContract $user, PanelContract $panel): bool {
         return true;
     }
 }
