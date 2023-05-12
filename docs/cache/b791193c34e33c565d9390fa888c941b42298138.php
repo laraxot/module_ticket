db4366384970b5e3f3afac3ace2939f38b4a63f9
marco76tv <marco.sottana@gmail.com>
2023-05-12 17:55:14 +0200

    up

--- docs/cache/b791193c34e33c565d9390fa888c941b42298138.php
+++ docs/cache/b791193c34e33c565d9390fa888c941b42298138.php
@@ -1,5 +1,5 @@
 <li class="pl-4">
-    <a href="<?php echo e($item->getPath()); ?>"
+    <a href="<?php echo e(url($item->getPath())); ?>"
         class="<?php echo e('lvl' . $level); ?> <?php echo e($page->isItemActive($item) ? 'active font-semibold text-blue-500' : ''); ?> nav-menu__item hover:text-blue-500"
         >
         <?php echo e($item->title); ?>
