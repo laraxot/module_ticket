660dcd8768a382bb0c8479f2a3b95c24aaf7b438
Malebestia <vair81@gmail.com>
2023-11-16 10:35:23 +0100

    up

--- Resources/views/livewire/ticket/create/create.blade.php
+++
@@ -1,5 +0,0 @@
-<div>
-    <x-input.group type="text" name="title" />
-    <x-input.group type="address" name="place" />
-    <button type="button" wire:click="store()" class="btn btn-info">TEST</button>
-</div>
--- Resources/views/livewire/ticket/create/inserire_dati_specifici.blade.php
+++
@@ -1,7 +0,0 @@
-<div>
-    <x-input.group type="text" name="title" />
-    <x-input.group type="address" name="place" />
-    <button type="button" wire:click="save()" class="btn btn-info">Save</button>
-
-    <x-flash-message />
-</div>
