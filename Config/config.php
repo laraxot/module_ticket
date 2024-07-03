7f74d85c9d0e4a343925ffe254e21ed8880829b8
Malebestia <vair81@gmail.com>
2024-07-03 16:06:44 +0200

    .

--- Config/config.php
+++ Config/config.php
@@ -4,8 +4,8 @@ declare(strict_types=1);
 
 return [
     'name' => 'Ticket',
-    //'icon' => 'heroicon-o-ticket',
-    'icon' => 'heroicon-o-magnifying-glasslifebuoy',
+    'icon' => 'heroicon-o-ticket',
+    // 'icon' => 'heroicon-o-magnifying-glasslifebuoy',
 
     'navigation_sort' => 1,
 ];
