d8c8ba1b0c553ab5960715155cccb069b47ad2db
marco <marco.sottana@gmail.com>
2024-07-12 16:11:13 +0200

    .

--- Resources/views/components/blocks/ticket_list/map.blade.php
+++ Resources/views/components/blocks/ticket_list/map.blade.php
@@ -3,8 +3,7 @@
     
     @livewire(\Modules\Geo\Filament\Widgets\OSMMapWidget::class)
     @livewire(\Modules\Geo\Filament\Widgets\LocationMapTableWidget::class)
-    --}}
-    
+    --}}    
     
     @livewire(\Modules\Ticket\Filament\Widgets\TicketsMapTableWidget::class)
 </div>
