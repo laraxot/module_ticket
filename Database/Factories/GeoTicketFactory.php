d589856f13d7ea84f70261394b25bcdf9e341043
marco76tv <marco.sottana@gmail.com>
2024-08-06 08:58:59 +0200

    up

--- Database/Factories/GeoTicketFactory.php
+++ Database/Factories/GeoTicketFactory.php
@@ -5,24 +5,19 @@ declare(strict_types=1);
 namespace Modules\Ticket\Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Illuminate\Database\Eloquent\Model;
-use Modules\Ticket\Models\GeoTicket;
 
 class GeoTicketFactory extends Factory
 {
     /**
      * The name of the factory's corresponding model.
-     *
-     * @var class-string<Model>
      */
-    protected $model = GeoTicket::class;
+    protected $model = \Modules\Ticket\Models\GeoTicket::class;
 
     /**
      * Define the model's default state.
      */
     public function definition(): array
     {
-        return [
-        ];
+        return [];
     }
 }
