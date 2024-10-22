5b60c08131ebd574909eba32fd61b3a66b98d481
Malebestia <vair81@gmail.com>
2024-10-22 17:33:20 +0200

    fix(create-ticket.blade.php): add color attribute to submit button feat(FilterCoordinatesInRadius.php): remove debug statement and improve validation logic

--- Resources/views/filament/widgets/create-ticket.blade.php
+++ Resources/views/filament/widgets/create-ticket.blade.php
@@ -10,6 +10,7 @@
             class="w-full py-4 bg-blue-500 hover:bg-blue-700"
             type="submit"
             form="create"
+            color="danger"
         >
         {{ __('ticket::txt.click-here-to-submit-a-new-ticket') }}
         </x-filament::button>
